a971881e77c25755d577cc7c4bfd5ff7682ff2df
Disezmike <mikenatchapon@gmail.com>
2025-09-27 20:38:05 +0700

    add localStorage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:localstorage/localstorage.dart';
 import 'package:waiwan/screens/start_screen.dart';
 import 'package:waiwan/utils/colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await initLocalStorage();
   runApp(const MyApp());
 }
 
