caca19d45c6950ce54e659c8a5bfee46bc48bec4
Disezmike <mikenatchapon@gmail.com>
2025-10-14 03:18:09 +0700

    add upload profile

--- lib/screens/profilescreen/edit_profile.dart
+++ lib/screens/profilescreen/edit_profile.dart
@@ -1,12 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:waiwan/model/user.dart';
+import 'package:waiwan/services/user_service.dart';
+import 'package:waiwan/utils/helper.dart';
 import '../../widgets/user_profile/edit_profile_image.dart';
 import '../../widgets/user_profile/profile_form.dart';
 import '../../widgets/user_profile/save_button.dart';
 
 class EditProfile extends StatefulWidget {
   final User user;
-  EditProfile({super.key, required this.user});
+  const EditProfile({super.key, required this.user});
 
   @override
   State<EditProfile> createState() => _EditProfileState();
@@ -22,6 +30,11 @@ class _EditProfileState extends State<EditProfile> {
       TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   // final TextEditingController _genderController = TextEditingController();
+
+  final ImagePicker _picker = ImagePicker();
+  XFile? _pickedImage;
+  File? _cropedImage;
+  Uint8List? _pickedBytes;
 
   @override
   void initState() {
@@ -58,6 +71,72 @@ class _EditProfileState extends State<EditProfile> {
     super.dispose();
   }
 
+  Future<void> _pickFromGallery() async {
+    try {
+      final XFile? photo = await _picker.pickImage(source: ImageSource.gallery);
+      final croppedFile = await _cropImage(imageFile: File(photo!.path));
+      if (croppedFile != null) {
+        final bytes = await croppedFile.readAsBytes();
+        setState(() {
+          _pickedImage = photo;
+          _pickedBytes = bytes;
+          _cropedImage = croppedFile;
+        });
+      }
+    } catch (e) {
+      debugPrint('Error picking image: $e');
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('ไม่สามารถเลือกภาพได้: $e')));
+      }
+    }
+  }
+
+  Future<File?> _cropImage({required File imageFile}) async {
+    try {
+      CroppedFile? croppedImg = await ImageCropper().cropImage(
+        maxWidth: 512,
+        maxHeight: 512,
+        sourcePath: imageFile.path,
+        compressQuality: 100,
+        aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1),
+        uiSettings: [WebUiSettings(context: context)]
+      );
+      if (croppedImg == null) {
+        return null;
+      } else {
+        return File(croppedImg.path);
+      }
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  Future<void> _onConfirm() async {
+    if (_cropedImage == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('กรุณาเลือกรูปก่อนกดยืนยัน')),
+      );
+      return;
+    }
+
+    final File file = File(_cropedImage!.path);
+    UserService().uploadProfileImage(file).catchError((e) {
+      if (mounted) {
+        showErrorSnackBar(context, 'อัพโหลดรูปไม่สำเร็จ');
+      }
+      debugPrint('Context is not mounted. Cannot show error SnackBar.');
+    });
+
+    if (mounted) {
+      showSuccessSnackBar(context, 'อัพโหลดรูปสำเร็จ');
+    } else {
+      debugPrint('Context is not mounted. Cannot show success SnackBar.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,11 +161,18 @@ class _EditProfileState extends State<EditProfile> {
             children: [
               // Profile Image Section
               EditProfileImage(
-                imageAsset: widget.user.profile.imageUrl ??
-                    'https://placehold.co/600x400.png',
-                onEditPressed: () {
-                  // TODO: Implement image selection
-                  print('Edit profile image pressed');
+                imageAsset: widget.user.profile.imageUrl!,
+                onEditPressed: () async {
+                  await _pickFromGallery();
+
+                  if (_cropedImage != null && mounted) {
+                    await _onConfirm();
+                    Timer(
+                      Duration(milliseconds: 500),
+                      () => Navigator.pop(context),
+                    );
+                    // Navigator.pop(context);
+                  }
                 },
               ),
 
@@ -98,7 +184,7 @@ class _EditProfileState extends State<EditProfile> {
                 address: '${_currentAddressController.text}',
                 onNameChanged: (value) {
                   // TODO: Handle name change
-                  print('Name changed: $value');
+                  debugPrint('Name changed: $value');
                   final name = value.split(' ');
                   if (name.isNotEmpty) {
                     _firstnameController.text = name[0];
@@ -109,11 +195,11 @@ class _EditProfileState extends State<EditProfile> {
                 },
                 onPhoneChanged: (value) {
                   // TODO: Handle phone change
-                  print('Phone changed: $value');
+                  debugPrint('Phone changed: $value');
                 },
                 onAddressChanged: (value) {
                   // TODO: Handle address change
-                  print('Address changed: $value');
+                  debugPrint('Address changed: $value');
                 },
               ),
 
