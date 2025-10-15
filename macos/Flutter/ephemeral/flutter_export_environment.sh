2ae7704ee16b916dd95ff326cb2113c2f4fbceed
Disezmike <mikenatchapon@gmail.com>
2025-10-15 10:24:56 +0700

    fix not load job

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\user\Documents\GitHub\waiwan"
+export "FLUTTER_ROOT=/Users/disezmike/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/disezmike/Development/projects/waiwan"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
